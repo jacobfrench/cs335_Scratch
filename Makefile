d6fce641fbf0e080e094179353f4ba4ab73a34d0
Keith Harryman <keithharryman75@gmail.com>
2015-10-23 22:47:35 -0700

    final first version commit

--- Makefile
+++ Makefile
@@ -2,10 +2,19 @@ CFLAGS = -I ./include
 ##LIB    = ./libggfonts.so
 LFLAGS = -lrt -lX11 -lGLU -lGL -pthread -lm #-lXrandr
 
-all: battlepong
+all: battlepong fonts.o fonttex.o
 
-battlepong: battlepong.cpp ppm.c log.c mytime.cpp
+battlepong: battlepong.cpp ppm.c log.c mytime.cpp fonts.o fonttex.o
 	g++ $(CFLAGS) battlepong.cpp log.c mytime.cpp jacobF.cpp fonts.o fonttex.o -Wall -Wextra $(LFLAGS) -obattlepong
+
+fonts.o: fonts.cpp fonts.h defs.h
+	g++ -c -Wall -Wextra fonts.cpp
+
+fonttex.o: fonttex.cpp
+	g++ -c -Wall -Wextra fonttex.cpp
+
 clean:
 	rm -f battlepong
+	rm -f fonttex.o
+	rm -f fonts.o
 
--- defs.h
+++ defs.h
@@ -39,83 +39,16 @@ typedef struct t_mouse {
 	int lbuttondown;
 } Mouse;
 
-typedef struct t_bsphere {
-	Vec center;
-	Flt radius;
-	Flt radiusSqr;
-} Bsphere;
-
-typedef struct t_bbox {
-	Flt min[3];
-	Flt max[3];
-} Bbox;
-
 typedef struct t_texmap {
 	int xres, yres;
 	unsigned char *c;
 	unsigned int *i;
 } Texmap;
 
-typedef struct t_screen {
+typedef struct t_myscreen {
 	int x_res, y_res;
-	int screen_x_res, screen_y_res;
-	float gl_perspective_viewangle;
 	Flt xcenter, ycenter;
-	Flt fy_res, fx_res;
-} Screen;
-
-#define MAX_LIGHTS (4)
-typedef struct t_lights {
-	int onoff;
-	int nlights;
-	Vec light;
-	float glAmb[4];
-	float glDif[4];
-	float glPos[4];
-} Lights;
-
-typedef struct t_keys {
-	int autorepeatstate, shiftstate, ctrlstate, shiftlockstate;
-} Keys;
-
-typedef struct t_camera {
-	Vec pos;
-	Vec move;
-	Vec dir;
-	Vec rot;
-	Flt ang;
-	Flt tilt;
-	Matrix m;
-} Camera;
-
-typedef struct t_mats {
-	Matrix worldSpace;
-	Matrix cameraSpace;
-	Matrix playerSpace;
-} Mats;
-
-typedef struct t_frustum {
-	Flt frontPlaneDistance;
-	Vec frontPlaneNormal;
-	Vec frontPlanePoint;
-	Flt farPlaneDistance;
-	Vec farPlanePoint;
-	Flt farObjectPlaneDistance;
-	Vec farObjectPlanePoint;
-	Flt missile_view_distance2;
-	//#1 is left   plane normal
-	//#2 is right  plane normal
-	//#3 is top    plane normal
-	//#4 is bottom plane normal
-	//#5 is front  plane normal
-	//#6 is far    plane normal
-	Vec planes[6];
-	Flt glplanesd[6][4];
-	float glplanesf[6][4];
-	float glFarPlaneDistance;
-	float glNearPlaneDistance;
-	Vec save_planes[6];
-} Frustum;
+} myScreen;
 
 #endif
 
