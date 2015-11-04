89ce612b524b69d431d368c90d8a91de5c1a6c28
briancas20 <briancasromero55@yahoo.com>
2015-11-03 18:14:53 -0800

    d

--- brianC.cpp
+++ brianC.cpp
@@ -12,64 +12,86 @@
 #include <stdlib.h>
 #include <AL/alut.h>
 
-
-
-int initAlut()
+ALuint alSource;
+ALuint alBuffer;
+void createSound(int trackNo)
 {
     //Get started right here.
     alutInit(0, NULL);
     if (alGetError() != AL_NO_ERROR) {
         printf("ERROR: alutInit()\n");
-        return 0;
     }
-   /* //Clear error state.
+    // // Clear error state.
     alGetError();
-    //
-    //Setup the listener.
-    //Forward and up vectors are used.
+
+    // // Setup the listener.
+    // // Forward and up vectors are used.
     float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
     alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
     alListenerfv(AL_ORIENTATION, vec);
     alListenerf(AL_GAIN, 1.0f);
-    //
-    //Buffer holds the sound information.
-    ALuint alBuffer;
-    alBuffer = alutCreateBufferFromFile("./pong.wav");
-    //
-    //Source refers to the sound.
-    ALuint alSource;
-    //Generate a source, and store it in a buffer.
+
+    // // Buffer holds the sound information.
+    // ALuint alBuffer;
+    //alBuffer = alutCreateBufferFromFile("./boing.wav");
+
+    switch(trackNo) {
+        case 1:
+            alBuffer = alutCreateBufferFromFile("./paddle1.wav");
+            break;
+        case 2:
+            alBuffer = alutCreateBufferFromFile("./blopp_converted.wav");
+            break;
+        case 3:
+            alBuffer = alutCreateBufferFromFile("./pad1a.wav");
+            break;
+       case 4:
+            alBuffer = alutCreateBufferFromFile("./pad2.wav");
+            break;
+     
+    }
+
+
+    // // Source refers to the sound.
+    // // ALuint alSource;
+    // // Generate a source, and store it in a buffer.
     alGenSources(1, &alSource);
     alSourcei(alSource, AL_BUFFER, alBuffer);
-    //Set volume and pitch to normal, no looping of sound.
+    // // Set volume and pitch to normal, no looping of sound.
     alSourcef(alSource, AL_GAIN, 1.0f);
     alSourcef(alSource, AL_PITCH, 1.0f);
     alSourcei(alSource, AL_LOOPING, AL_FALSE);
     if (alGetError() != AL_NO_ERROR) {
         printf("ERROR: setting source\n");
-        return 0;
     }
-    for (int i=0; i<4; i++) {
-        alSourcePlay(alSource);
-        usleep(250000);
-    }
-    //Cleanup.
-    //First delete the source.
-    alDeleteSources(1, &alSource);
-    //Delete the buffer.
-    alDeleteBuffers(1, &alBuffer);
-    //Close out OpenAL itself.
-    //Get active context.
-    ALCcontext *Context = alcGetCurrentContext();
-    //Get device for active context.
-    ALCdevice *Device = alcGetContextsDevice(Context);
-    //Disable context.
-    alcMakeContextCurrent(NULL);
-    //Release context(s).
-    alcDestroyContext(Context);
-    //Close device.
-    alcCloseDevice(Device);*/
-    return 0;
+//    for (int i=0; i<4; i++) {
+  //      while(true){
+            alSourcePlay(alSource);
+           // usleep(900000000);
+        
+    
+}
+
+void cleanSound()
+{
+    // // }
+    // // Cleanup.
+    // // First delete the source.
+alDeleteSources(1, &alSource);
+// // Delete the buffer.
+alDeleteBuffers(1, &alBuffer);
+// // Close out OpenAL itself.
+// // Get active context.
+ALCcontext *Context = alcGetCurrentContext();
+// // Get device for active context.
+ALCdevice *Device = alcGetContextsDevice(Context);
+// // Disable context.
+alcMakeContextCurrent(NULL);
+// // Release context(s).
+alcDestroyContext(Context);
+// // Close device.
+alcCloseDevice(Device);
+
 }
 
 #endif
